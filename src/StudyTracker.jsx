bdaba2bf1d6596ef2e09285f029a65bb4a47d22b
maron-1003 <ld.dragon3182@gmail.com>
2026-06-06 14:10:44 +0900

    ランキング機能追加

--- src/StudyTracker.jsx
+++ src/StudyTracker.jsx
@@ -688,7 +688,6 @@ export default function StudyTracker({ user, onLogout }) {
   if (showPast) {
     return <PastGoals user={user} onBack={() => setShowPast(false)} />;
   }
-  console.log("userMap:", userMap);
 
   return (
     <div className="min-h-screen bg-gray-900 text-white p-6 flex flex-col items-center">
