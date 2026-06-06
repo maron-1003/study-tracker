e348f2045523654dd13d7556f4c7cf0f2ac0a11c
maron-1003 <ld.dragon3182@gmail.com>
2026-06-06 10:16:52 +0900

    過去目標機能追加

--- src/StudyTracker.jsx
+++ src/StudyTracker.jsx
@@ -598,7 +598,7 @@ export default function StudyTracker({ user, onLogout }) {
           </h2>
 
           {selectedRecords.length > 0 ? (
-            <div className="w-full" style={{ height: "180px" }}>
+            <div className="w-full" style={{ height: "220px" }}>
               <Doughnut
                 data={dailyChartData}
                 options={{
